925579661e7b0dbefbe954e1853a81a2c73f0289
YRHG <975816342hhz@gmail.com>
2025-05-22 22:05:03 +0900

    view images updata

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -6,7 +6,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
-    <link rel="icon" type="image/x-icon" href="/Library/WebServer/Documents/laravel-bbs/laravel-bbs-20250520/public/images/纯阳.png">
+    <link rel="icon" type="image/png" href="{{ asset('images/favicon.png') }}">
 
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
