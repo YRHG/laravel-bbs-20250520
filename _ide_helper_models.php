b7ce470c5eb1f4a163d39c838b1f94075909429f
YRHG <975816342hhz@gmail.com>
2025-05-26 00:14:32 +0900

    监听 重置密码 用户修改

---
+++ _ide_helper_models.php
@@ -0,0 +1,44 @@
+<?php
+
+// @formatter:off
+// phpcs:ignoreFile
+/**
+ * A helper file for your Eloquent Models
+ * Copy the phpDocs from this file to the correct Model,
+ * And remove them from this file, to prevent double declarations.
+ *
+ * @author Barry vd. Heuvel <barryvdh@gmail.com>
+ */
+
+
+namespace App\Models{
+/**
+ * 
+ *
+ * @property int $id
+ * @property string $name
+ * @property string $email
+ * @property Carbon|null $email_verified_at
+ * @property string $password
+ * @property string|null $remember_token
+ * @property Carbon|null $created_at
+ * @property Carbon|null $updated_at
+ * @property-read DatabaseNotificationCollection<int, DatabaseNotification> $notifications
+ * @property-read int|null $notifications_count
+ * @method static UserFactory factory($count = null, $state = [])
+ * @method static Builder<static>|User newModelQuery()
+ * @method static Builder<static>|User newQuery()
+ * @method static Builder<static>|User query()
+ * @method static Builder<static>|User whereCreatedAt($value)
+ * @method static Builder<static>|User whereEmail($value)
+ * @method static Builder<static>|User whereEmailVerifiedAt($value)
+ * @method static Builder<static>|User whereId($value)
+ * @method static Builder<static>|User whereName($value)
+ * @method static Builder<static>|User wherePassword($value)
+ * @method static Builder<static>|User whereRememberToken($value)
+ * @method static Builder<static>|User whereUpdatedAt($value)
+ * @mixin \Eloquent
+ */
+	class User extends \Eloquent implements \Illuminate\Contracts\Auth\MustVerifyEmail {}
+}
+
---
+++ app/Http/Controllers/UsersController.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests\UserRequest;
+use App\Models\User;
+use Illuminate\Contracts\View\View;
+use Illuminate\Http\RedirectResponse;
+
+class UsersController extends Controller
+{
+    /**
+     * Show the user's profile.
+     *
+     * @param User $user
+     * @return View
+     */
+    public function show(User $user): View
+    {
+        return view('users.show', compact('user'));
+    }
+
+    /**
+     * Show the form for editing the user's profile.
+     *
+     * @param User $user
+     * @return View
+     */
+    public function edit(User $user): View
+    {
+        return view('users.edit', compact('user'));
+    }
+
+    /**
+     * Update the user's profile.
+     *
+     * @param UserRequest $request
+     * @param User $user
+     * @return RedirectResponse
+     */
+    public function update(UserRequest $request, User $user): RedirectResponse
+    {
+        $user->update($request->all());
+        return redirect()->route('users.show', $user->id)->with('success', 'Profile updated successfully.');
+    }
+}
