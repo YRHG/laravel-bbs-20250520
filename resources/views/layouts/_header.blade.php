26b50f109cae16c603285c06a86ed4673899a93c
YRHG <975816342hhz@gmail.com>
2025-05-29 22:34:53 +0900

    修改细节 views header topics

--- resources/views/layouts/_header.blade.php
+++ resources/views/layouts/_header.blade.php
@@ -1,4 +1,3 @@
-
 @php
     use App\Models\Category;
 
@@ -17,7 +16,6 @@
 
 <nav class="navbar navbar-expand-lg navbar-light bg-light navbar-static-top">
     <div class="container">
-        <!-- Branding Image 左边 -->
         <a class="navbar-brand" href="{{ url('/') }}">
             {{ config('app.name', '纯阳宫') }}
         </a>
@@ -27,31 +25,30 @@
             <span class="navbar-toggler-icon"></span>
         </button>
 
-        <div class="collapse navbar-collapse justify-content-between" id="navbarSupportedContent">
+        {{-- 移除了 justify-content-between --}}
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
 
-            <!-- 左侧空ul（或者你之前的菜单） -->
-                <ul class="navbar-nav mr-auto">
-                    <li class="nav-item "><a class="nav-link {{ request()->routeIs('topics.index') ? 'active' : '' }}" href="{{ route('topics.index') }}">话题</a></li>
-                    @if($categories->count())
-                        @foreach($categories as $category)
-                            <li class="nav-item">
-                                <a class="nav-link {{ (request()->routeIs('categories.show') && $currentCategoryId == $category->id) ? 'active' : '' }}"
-                                   href="{{ route('categories.show', $category->id) }}">{{ __($category->name) }}</a>
-                            </li>
-                        @endforeach
-                    @endif
-                </ul>
+            {{-- 使用 me-auto (margin-end: auto) 替代 mr-auto for BS5 --}}
+            <ul class="navbar-nav me-auto">
+                <li class="nav-item"><a class="nav-link {{ request()->routeIs('topics.index') ? 'active' : '' }}" href="{{ route('topics.index') }}">话题</a></li>
+                @if($categories->count())
+                    @foreach($categories as $category)
+                        <li class="nav-item">
+                            <a class="nav-link {{ (request()->routeIs('categories.show') && $currentCategoryId == $category->id) ? 'active' : '' }}"
+                               href="{{ route('categories.show', $category->id) }}">{{ __($category->name) }}</a>
+                        </li>
+                    @endforeach
+                @endif
+            </ul>
 
-
-                <!-- 中间图标 -->
-            <div class="d-flex justify-content-center flex-grow-1">
+            {{-- 使用 mx-auto 使这个div在其兄弟元素me-auto和ms-auto之间居中 --}}
+            {{-- 移除了 d-flex justify-content-center flex-grow-1 --}}
+            <div class="mx-auto">
                 <img src="{{ asset('images/JX3_Online_logo.jpg') }}" alt="Logo" style="height:40px;">
-
             </div>
 
-            <!-- 右侧用户信息 -->
-            <ul class="navbar-nav navbar-right">
-                <!-- 认证相关链接 -->
+            {{-- 使用 ms-auto (margin-start: auto) 替代 navbar-right for BS5 --}}
+            <ul class="navbar-nav ms-auto">
                 @guest
                     <li class="nav-item"><a class="nav-link" href="{{ route('login') }}">{{ __('登录') }}</a></li>
                     <li class="nav-item"><a class="nav-link" href="{{ route('register') }}">{{ __('注册') }}</a></li>
@@ -67,17 +64,17 @@
                                  class="img-responsive img-circle" width="30px" height="30px" alt="头像">
                             {{ auth()->user()->name }}
                         </button>
-
-                        <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+                        {{-- 添加了 dropdown-menu-end 使菜单在右侧时正确对齐 --}}
+                        <div class="dropdown-menu dropdown-menu-end" aria-labelledby="navbarDropdown">
                             <a class="dropdown-item"
                                href="{{ route('users.show', auth()->user()) }}">{{ __('个人资料') }}</a>
                             <a class="dropdown-item"
                                href="{{ route('users.edit', auth()->user()) }}">{{ __('编辑资料') }}</a>
                             <div class="dropdown-divider"></div>
                             <a class="dropdown-item" id="logout" href="#">
-                                <form action="{{ route('logout') }}" method="POST">
+                                <form action="{{ route('logout') }}" method="POST" style="display: block;"> {{-- display: block or inline-block might be better for full width clickable area --}}
                                     @csrf
-                                    <button class="btn btn-block btn-danger" type="submit" name="button">{{ __('退出登录') }}</button>
+                                    <button class="btn btn-block btn-danger w-100" type="submit" name="button">{{ __('退出登录') }}</button>
                                 </form>
                             </a>
                         </div>
--- resources/views/topics/index.blade.php
+++ resources/views/topics/index.blade.php
@@ -1,3 +1,10 @@
+@php
+    use Illuminate\Support\Facades\Request;
+
+    $routeHasQueryOrder = Request::has('order');
+    $currentOrder = Request::get('order', 'default'); // 默认值为 'default'
+
+@endphp
 @extends('layouts.app')
 
 @section('title', isset($category) ? $category->name : __('Topics'))
@@ -6,7 +13,6 @@
 
     <div class="row mb-5">
         <div class="col-lg-9 col-md-9 topic-list">
-
             @if (isset($category))
                 <div class="alert alert-info" role="alert">
                     {{ $category->name }} ：{{ $category->description }}
@@ -14,10 +20,22 @@
             @endif
 
             <div class="card ">
+
                 <div class="card-header bg-transparent">
                     <ul class="nav nav-pills">
-                        <li class="nav-item"><a class="nav-link active" href="#">最終返信</a></li>
-                        <li class="nav-item"><a class="nav-link" href="#">最新投稿</a></li>
+                        <li class="nav-item">
+                            @if($routeHasQueryOrder)
+                                <a class="nav-link {{ $currentOrder == 'default' ? 'active' : ''}}"
+                                   href="{{ Request::url() }}?order=default">{{ __('最終返信') }}</a>
+                            @else
+                                <a class="nav-link active"
+                                   href="{{ Request::url() }}?order=default">{{ __('最終返信') }}</a>
+                            @endif
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link {{ $routeHasQueryOrder && $currentOrder == 'recent' ? 'active' : '' }}"
+                               href="{{ Request::url() }}?order=recent">{{ __('最新投稿') }}</a>
+                        </li>
                     </ul>
                 </div>
 
@@ -26,7 +44,7 @@
                     @include('topics._topic_list', ['topics' => $topics])
                     {{-- 分页 --}}
                     <div class="mt-5">
-                        {!! $topics->appends(\Illuminate\Support\Facades\Request::except('page'))->render() !!}
+                        {!! $topics->appends(Request::except('page'))->render() !!}
                     </div>
                 </div>
             </div>
