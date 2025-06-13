5affddd8cbc9db5966b3a395d591c5799a079d52
YRHG <975816342hhz@gmail.com>
2025-06-13 17:50:55 +0900

    安装 lab404/laravel-impersonate 用来测试不同用户的操作权限

--- app/Models/User.php
+++ app/Models/User.php
@@ -15,20 +15,24 @@ use Illuminate\Notifications\DatabaseNotification;
 use Illuminate\Notifications\DatabaseNotificationCollection;
 use Illuminate\Notifications\Notifiable;
 use Illuminate\Support\Carbon;
+use Lab404\Impersonate\Models\Impersonate;
+use Spatie\Permission\Models\Permission;
+use Spatie\Permission\Models\Role;
+use Spatie\Permission\Traits\HasRoles;
 
 /**
  *
  *
- * @property int $id
- * @property string $name
- * @property string $email
- * @property Carbon|null $email_verified_at
- * @property string $password
- * @property string|null $remember_token
- * @property Carbon|null $created_at
- * @property Carbon|null $updated_at
- * @property-read DatabaseNotificationCollection<int, DatabaseNotification> $notifications
- * @property-read int|null $notifications_count
+ * @property int $id 用户 ID
+ * @property string $name 用户名
+ * @property string $email 邮箱
+ * @property Carbon|null $email_verified_at 邮箱验证时间
+ * @property string $password 密码
+ * @property string|null $remember_token 记住我 Token
+ * @property Carbon|null $created_at 创建时间
+ * @property Carbon|null $updated_at 更新时间
+ * @property-read DatabaseNotificationCollection<int, DatabaseNotification> $notifications 用户通知集合
+ * @property-read int|null $notifications_count 通知数量
  * @method static UserFactory factory($count = null, $state = [])
  * @method static Builder<static>|User newModelQuery()
  * @method static Builder<static>|User newQuery()
@@ -45,37 +49,46 @@ use Illuminate\Support\Carbon;
  * @property string|null $introduction 个人简介
  * @method static Builder<static>|User whereAvatar($value)
  * @method static Builder<static>|User whereIntroduction($value)
- * @property-read Collection<int, Topic> $topics
- * @property-read int|null $topics_count
- * @property-read Collection<int, Reply> $replies
- * @property-read int|null $replies_count
- * @property int $notification_count
+ * @property-read Collection<int, Topic> $topics 用户发布的话题集合
+ * @property-read int|null $topics_count 用户话题数量
+ * @property-read Collection<int, Reply> $replies 用户的回复集合
+ * @property-read int|null $replies_count 回复数量
+ * @property int $notification_count 未读通知数量
  * @method static Builder<static>|User whereNotificationCount($value)
+ * @property-read Collection<int, Permission> $permissions 用户权限集合
+ * @property-read int|null $permissions_count 权限数量
+ * @property-read Collection<int, Role> $roles 用户角色集合
+ * @property-read int|null $roles_count 角色数量
+ * @method static Builder<static>|User permission($permissions, $without = false)
+ * @method static Builder<static>|User role($roles, $guard = null, $without = false)
+ * @method static Builder<static>|User withoutPermission($permissions)
+ * @method static Builder<static>|User withoutRole($roles, $guard = null)
  * @mixin \Eloquent
  */
 class User extends Authenticatable implements MustVerifyEmail
 {
     /** @use HasFactory<UserFactory> */
-    use HasFactory, MustVerifyEmailTrait;
+    use HasFactory, MustVerifyEmailTrait, HasRoles, Impersonate;
 
     use Notifiable {
         notify as protected laravelNotify;
     }
 
     /**
-     * Rewrite the notify method to handle notifications.
+     * 重写 notify 方法，用于处理通知逻辑
      *
-     * @param $instance
+     * @param $instance 通知实例
      * @return void
      */
     public function notify($instance): void
     {
-        // If the notification is a VerifyEmail notification, we don't want to notify the user and if the notification is for the current user.
+        // 如果是当前用户，且不是邮箱验证通知，则不执行通知
         if ($this->id === auth()->id() && get_class($instance) !== VerifyEmail::class) {
             return;
         }
 
-        // 只有数据库类型的通知才需要提醒, 直接发送 Email 或者其他的都不需要增加通知计数
+        // 仅当通知支持 toDatabase 方法时才计数（即为数据库通知），
+        // Email 或其他方式通知不增加计数
         if (method_exists($instance, 'toDatabase')) {
             $this->increment('notification_count');
         }
@@ -84,7 +97,7 @@ class User extends Authenticatable implements MustVerifyEmail
     }
 
     /**
-     * The attributes that are mass assignable.
+     * 可批量赋值的字段
      *
      * @var list<string>
      */
@@ -97,7 +110,7 @@ class User extends Authenticatable implements MustVerifyEmail
     ];
 
     /**
-     * The attributes that should be hidden for serialization.
+     * 序列化时需要隐藏的字段
      *
      * @var list<string>
      */
@@ -107,7 +120,7 @@ class User extends Authenticatable implements MustVerifyEmail
     ];
 
     /**
-     * Get the attributes that should be cast.
+     * 属性类型转换
      *
      * @return array<string, string>
      */
@@ -120,7 +133,7 @@ class User extends Authenticatable implements MustVerifyEmail
     }
 
     /**
-     * User has many topic.
+     * 用户拥有多个话题
      *
      * @return HasMany
      */
@@ -130,7 +143,7 @@ class User extends Authenticatable implements MustVerifyEmail
     }
 
     /**
-     * Check if the user is the author of the given model.
+     * 判断用户是否为模型的作者
      *
      * @param $model
      * @return bool
@@ -141,7 +154,7 @@ class User extends Authenticatable implements MustVerifyEmail
     }
 
     /**
-     * User has many replies.
+     * 用户拥有多个回复
      *
      * @return HasMany
      */
@@ -151,7 +164,7 @@ class User extends Authenticatable implements MustVerifyEmail
     }
 
     /**
-     * Mark all notifications as read.
+     * 标记所有通知为已读
      *
      * @return void
      */
